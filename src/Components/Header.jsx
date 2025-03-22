703344b34e819f2a321d17d54a23fc0fe8c56675
Aymen07171 <elattarayman1@gmail.com>
2025-03-22 01:11:52 +0000

    'Fix Code '

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <header  className='text-white z-2 relative'>
-            <h1>My Website</h1>
-            <nav>
+            <h1>3D Circle </h1>
+            <nav className=''>
                 <ul>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
